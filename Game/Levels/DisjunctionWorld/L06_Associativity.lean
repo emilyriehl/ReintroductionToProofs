dfac93a190797ced31da178aa99979371e0db75b
emilyriehl <eriehl@jhu.edu>
2025-09-15 09:45:03 -0400

    better documentation

--- Game/Levels/DisjunctionWorld/L06_Associativity.lean
+++ Game/Levels/DisjunctionWorld/L06_Associativity.lean
@@ -11,7 +11,12 @@ Lean uses `P ∨ Q ∨ R` as an abbreviation for `P ∨ (Q ∨ R)`.
 
 Given a hypothesis `h₁ : (P ∨ Q) ∨ R`, `cases h₁` will split the hypothesis into two cases: one assuming that `P ∨ Q` is true and the other assuming `R` is true.
 
-Similarly given a hypothesis `h₂ : P ∨ Q ∨ R`, `cases h₂` the hypothesis into two cases: one assuming that `P` is true and the other assuming `Q ∨ R` is true."
+Similarly given a hypothesis `h₂ : P ∨ Q ∨ R`, `cases h₂` the hypothesis into two cases: one assuming that `P` is true and the other assuming `Q ∨ R` is true.
+
+Note the `rcases` tactic allows more general patterns of disjunctions. Given `h₁ : (P ∨ Q) ∨ R` try `rcases h₁ with (p | q) | r`. Similarly, given `h₂ : P ∨ Q ∨ R`, try `rcases h₂ with p | q | r`.
+
+Aside: typing `\\7` using any single digit number produces a subscript with that number, like `₇`.
+"
 
 /-- Or is associative: `(P ∨ Q) ∨ R` is true if and only if `P ∨ (Q ∨ R)` is true.-/
 Statement {P Q R : Prop} : (P ∨ Q) ∨ R ↔ P ∨ Q ∨ R := by
@@ -35,9 +40,6 @@ Statement {P Q R : Prop} : (P ∨ Q) ∨ R ↔ P ∨ Q ∨ R := by
   exact Or.inl (Or.inr hq)
   exact Or.inr hr
 
-Conclusion "This proves the associativity of disjunction.
-
-Note the `rcases` tactic allows more general patterns of disjunctions. Given `h : (P ∨ Q) ∨ R` try `rcases h with (p | q) | r`. Similarly, given `k : P ∨ Q ∨ R`, try `rcases k with p | q | r`.
-"
+Conclusion "This proves the associativity of disjunction."
 
 /- Use these commands to add items to the game's inventory. -/
