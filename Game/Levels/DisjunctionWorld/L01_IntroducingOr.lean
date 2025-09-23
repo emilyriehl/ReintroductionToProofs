31cffac3b762317d65d0c43e0c61fb328f7263e6
Alex Kontorovich <58564076+AlexKontorovich@users.noreply.github.com>
2025-09-23 17:18:35 -0400

    proposed use of `left`/`right` tactics

--- Game/Levels/DisjunctionWorld/L01_IntroducingOr.lean
+++ Game/Levels/DisjunctionWorld/L01_IntroducingOr.lean
@@ -12,29 +12,29 @@ Thus under the hypothesis that `P` and `Q` are both true, there are two ways to 
 Note `exact p` or `exact q` won't work as these are proofs of different propositions."
 
 /--
-For propositions `P` and `Q`, `Or.inl` is a proof that `P → (P ∨ Q)`.
+For propositions `P` and `Q`, the `left` tactic converts a goal of `P ∨ Q` to a goal of `P`.
 -/
-TheoremDoc Or.inl as "inl" in "Or"
+TacticDoc left as "left" in "Or"
 
 /--
-For propositions `P` and `Q`, `Or.inr` is a proof that `Q → (P ∨ Q)`.
+For propositions `P` and `Q`, the `right` tactic converts a goal of `P ∨ Q` to a goal of `Q`.
 -/
-TheoremDoc Or.inr as "inr" in "Or"
+TacticDoc right as "right" in "Or"
 
 /-- There are two ways to prove `P ∨ Q` from the given hypotheses. Can you find them both?-/
 Statement {P Q : Prop} (p : P) (q : Q) : P ∨ Q := by
-  Hint "You have two choices: to reduce to the case of proving the left proposition or to reduce to the case of proving the right proposition. Type `apply Or.inl` to tell Lean you'd like too prove the left proposition or type `apply Or.inr` to tell Lean you'd like to prove the right proposition."
+  Hint "You have two choices: to reduce to the case of proving the left proposition or to reduce to the case of proving the right proposition. Type `left` to tell Lean you'd like to prove the left proposition or type `right` to tell Lean you'd like to prove the right proposition."
   Branch
-    apply Or.inl
+    left
     Hint (hidden := true) "What is the goal now?"
     exact p
   Branch
-    apply Or.inr
+    right
     Hint (hidden := true) "What is the goal now?"
     exact q
-  exact Or.inl p
+  left; exact p
 
 Conclusion "If this is your first time solving this level, delete your code and prove the same theorem another way."
 
 /- Use these commands to add items to the game's inventory. -/
-NewTheorem Or.inl Or.inr
+NewTactic left right
