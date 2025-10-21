f37671f46c6d947767ee6eac83b2ab12ff49e188
Emily Riehl <eriehl@jhu.edu>
2025-10-21 15:53:05 -0400

    broken; moved stuff

---
+++ Game/Levels/AdvancedFunctionWorld.lean
@@ -0,0 +1,28 @@
+import Game.Levels.QuantifierWorld.L01_UniversalTruth
+import Game.Levels.QuantifierWorld.L02_MoreTransitivity
+import Game.Levels.QuantifierWorld.L03_UniversalElimination
+import Game.Levels.QuantifierWorld.L04_ComposingInjectivity
+import Game.Levels.QuantifierWorld.L05_CancelingInjectivity
+import Game.Levels.QuantifierWorld.L06_DoubleNegation
+import Game.Levels.QuantifierWorld.L07_FunctionExtensionality
+import Game.Levels.QuantifierWorld.L08_IntroducingExistence
+import Game.Levels.QuantifierWorld.L09_TotalFunctions
+import Game.Levels.QuantifierWorld.L10_CancelingSurjectivity
+import Game.Levels.QuantifierWorld.L11_ComposingSurjectivity
+import Game.Levels.QuantifierWorld.L12_CommutingQuantifiers
+import Game.Levels.QuantifierWorld.L13_NegatingExistence
+import Game.Levels.QuantifierWorld.L14_NegatingUniversality
+import Game.Levels.QuantifierWorld.L15_BossLevel
+
+World "AdvancedFunctionWorld"
+Title "Advanced Function World"
+
+Introduction "In this world, we will return to our study of functions.
+
+Using the universal and existential quantifiers, we will state important properties of functions
+and prove the interrelationships between these notions.
+
+We will also answer the question of what it means for two functions `f g : A → B` to be equal.
+
+These results will be used in Equivalence World to answer what it means for two types to be the same.
+"
--- Game/Metadata/Tactic/Constructor.lean
+++ Game/Metadata/Tactic/Constructor.lean
@@ -10,6 +10,6 @@ namespace Game
 macro (name := constructor) "constructor" : tactic =>
   `(tactic| fconstructor)
 
-extend_docs Game.constructor after "The variant used in the Robo game is called
+extend_docs Game.constructor after "The variant used in the ReintroductionToProofs game is called
 `fconstructor` in Mathlib. The only difference is the real `constructor` sorts
 the goals in a way not ideal for the game."
