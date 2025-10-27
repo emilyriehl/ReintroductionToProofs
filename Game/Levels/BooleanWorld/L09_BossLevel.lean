333b50459009506b84c933df49d9820a1c08af1a
Emily Riehl <eriehl@jhu.edu>
2025-10-27 16:20:59 -0400

    tweaking boss level

--- Game/Levels/BooleanWorld/L09_BossLevel.lean
+++ Game/Levels/BooleanWorld/L09_BossLevel.lean
@@ -22,25 +22,28 @@ Solve the first three goals by `exact p`, `exact q`, and `exact r` &mdash; repla
 "
 
 /-- The following expression in conjunctive normal form is satisfiable. -/
-Statement (preamble := let p : Bool := ?_; let q : Bool := ?_; let r : Bool := ?_) : ∃ p : Bool, ∃ q : Bool, ∃ r : Bool,
+Statement (preamble := let p : Bool := ?_; let q : Bool := ?_; let r : Bool := ?_; rotate_right; rotate_right; rotate_right) : ∃ p : Bool, ∃ q : Bool, ∃ r : Bool,
 and (not p || q)
 (and (not q || p)
 (and (not p || not r)
 (and (not q || not r)
 (p || q)))) = true := by
-  Hint "For this level to work, we need to swap the goals. Type `pick_goal 4` to move the fourth goal to the front."
-  pick_goal 4
-  Hint "Now supply your conjectured value of `p` with `exact ??`."
-  exact true
-  Hint "Now type `pick_goal 3` to move the third goal to the front."
-  pick_goal 3
-  Hint "Now supply your conjectured value of `q` with `exact ??`."
-  exact true
-  Hint "Now type `pick_goal 2` to move the third goal to the front."
-  pick_goal 2
+  --  Hint "For this level to work, we need to swap the goals. Type `pick_goal 4` to move the fourth goal to the front."
+  -- pick_goal 4
   Hint "Now supply your conjectured value of `r` with `exact ??`."
   exact false
-  Hint "Now type ⟨p, q, r, rfl⟩` to use these values and attempt to solve the goal by `rfl`. If this works, congratulations! If not, go back and change the values for `p`, `q`, and `r`. Ignore the funny looking `∃` symbol in the formula, which will be explained in a future world."
+  -- Hint "Now type `pick_goal 3` to move the third goal to the front."
+  -- pick_goal 3
+  Hint "Now supply your conjectured value of `q` with `exact ??`."
+  exact true
+  -- Hint "Now type `pick_goal 2` to move the third goal to the front."
+  -- pick_goal 2
+  Hint "Now supply your conjectured value of `p` with `exact ??`."
+  exact true
+  --
+  Hint "Now type ⟨p, q, r, rfl⟩` to use these values and attempt to solve the goal by `rfl`.
+  If this works, congratulations! If not, go back and change the values for `p`, `q`, and `r`.
+  Ignore the funny looking `∃` symbol in the formula, which will be explained in a future world."
   exact ⟨p, q, r, rfl⟩
 
 Conclusion "If this was too easy, you might find it interesting to learn that the general problem of identifying whether a formula in conjunctive normal form is satisfiable is NP complete!"
