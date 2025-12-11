82579a29edbab241e2ca9a7882a44bea235f4f48
emilyriehl <eriehl@jhu.edu>
2025-12-11 14:30:23 +0100

    new boss level for type world

--- Game/Levels/TypeWorld/L05_TypeOfPropositions.lean
+++ Game/Levels/TypeWorld/L05_TypeOfPropositions.lean
@@ -1,28 +1,26 @@
 import Game.Metadata
 
 World "TypeWorld"
-Level 6
+Level 5
 
 Title "Type of Propositions"
 
-Introduction "Lean also has a built in type of propositions denoted `Prop`, which is now one of the types in the library of definitions.
+Introduction "Lean has a built in type of propositions denoted `Prop`.
 
-In the empty context, `Prop` contains propositions like `True`, which is the true proposition, with canonical proof `⟨⟩ : True`. This can now be found in the library of definitions.
+In the empty context, `Prop` contains propositions like `True`, which is the true proposition, with canonical proof `⟨⟩ : True`.
 
 In this level, we consider the context given by two propositions `P` and `Q` together with a proof `p : P` of `P`.
 
 Thus, `P` is a proposition that we have assumed to be true, while `Q` is a proposition that may or may not be true.
 
 Under these assumptions, either `P` or `Q` define elements of `Prop`. Use the `Retry` button to solve this level in multiple ways.
-
-Note that elements of `Prop` are considered to be at a smaller universe level than elements of `Type` and indeed we have `Prop : Type`.
 "
 
 /-- The type of propositions `Prop` contains propositions like `True`. -/
 Statement (P Q : Prop) (p : P) : Prop := by
   exact Q
 
-Conclusion "Move on to Function World to meet your first type forming operation."
+Conclusion "The proposition `True` and the type `Prop` are now in your library of definitions."
 
 /-- The type `Prop` is the type of propositions. -/
 DefinitionDoc «Prop» as "Prop" in "Type"
