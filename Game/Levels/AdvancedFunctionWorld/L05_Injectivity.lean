7406bbb785ae5ce4ae2aeaf5b7f445f2dd3251b5
JadAbouHawili <81527369+JadAbouHawili@users.noreply.github.com>
2026-05-09 15:16:26 +0300

    simpler solutions for injectivity level

--- Game/Levels/AdvancedFunctionWorld/L05_Injectivity.lean
+++ Game/Levels/AdvancedFunctionWorld/L05_Injectivity.lean
@@ -32,14 +32,8 @@ Statement Bool.not_injective : ∀ x y : Bool, not x = not y → x = y := by
   Hint (hidden := true) "This result can be proven in cases. Try `cases {x}` followed by `cases {y}`. You can pass to both cases at once by typing `cases {x} <;> cases {y}`"
   cases x <;> cases y
   rfl
-  Hint (hidden := true) "This case is considerably more difficult than the previous one because your goal is to prove that `false = true`. But you may recall from Boolean world that we can prove `¬ (false = true) := Bool.noConfusion` and `¬ (true = false) := Bool.noConfusion`. This suggests that the only way we will be able to prove `false = true` is by finding a contradition among our hypothesis. To set up this proof strategy, use the tactic `exfalso`."
-  exfalso
-  Hint (hidden := true) "Now where does the contradiction come from? We have exactly one hypothesis, namely `{p} : not false = not true`. Recall that `not false = true` and `not true = false`, with both of these equalities by definition. Thus `{p}` is also a proof of `true = false`. You can add this to your context by `have eq : true = false := p`."
-  have eq : true = false := p
-  Hint (hidden := true) "To get the contradiction, you should now add a proof of the negation of `{eq}`. You can do this with `have neq : ¬ (true = false) := Bool.noConfusion`."
-  have neq : ¬ (true = false) := Bool.noConfusion
-  Hint (hidden := true) "Revisit Negation World to remind yourself how to get an element of `False` from `{eq}` and `{neq}`."
-  exact neq eq
+  Hint (hidden := true) "{p} is a contradictory hypothesis, and so `cases {p}` will close the goal"
+  cases p
   Hint (hidden := true) "This case is similar to the previous one. Can you figure out how to solve it?"
   apply False.elim
   have eq : false = true := p
